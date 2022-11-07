a56026a3506a64c95e708bc4a144f6993b05f89d
Robinkim93 <kal6529@gmail.com>
2022-11-07 20:03:09 +0900

    [38기 김택수] ADD : 상세페이지 데이터 조회 완료 - MODIFY : categoryId 반환하는 key값 추가

--- models/productDao.js
+++ models/productDao.js
@@ -1,7 +1,8 @@
 const { database } = require("../util/dataSourceWrapperClass");
 
-const getNewProducts = async(offset, limit) => {
-     const data = await database.query(`
+const getNewProducts = async (offset, limit) => {
+  const data = await database.query(
+    `
      SELECT
           p.id AS productId,
           b.english_name AS brand,
@@ -28,9 +29,88 @@ const getNewProducts = async(offset, limit) => {
                GROUP BY product_id
           ) sp ON sp.product_id = p.id
           ORDER BY p.id DESC
-          LIMIT ?, ?`, [offset, limit]
-     );
-     return data;
+          LIMIT ?, ?`,
+    [offset, limit]
+  );
+  return data;
 };
 
-module.exports = { getNewProducts }
+const getProductInfo = async (productId) => {
+  const data = await database.query(`
+       SELECT
+         p.category_id AS categoryId, 
+         b.english_name AS brandEngName,
+         p.english_name AS productEngName,  
+         p.korean_name AS productKrName,
+         p.thumbnail AS productImage,
+         p.model_number AS modelNumber,
+         p.color,
+         p.released_price,
+         (SELECT bp.price FROM buy_prices AS bp WHERE bp.product_id = ${productId} ORDER BY bp.price ASC LIMIT 1) AS buyPrice,
+         (SELECT sp.price FROM sell_prices AS sp WHERE sp.product_id = ${productId} ORDER BY sp.price ASC LIMIT 1) AS sellPrice,
+         (SELECT dh.price FROM deal_histories AS dh WHERE dh.product_id = ${productId} ORDER BY dh.price ASC LIMIT 1) AS recentlyPrice
+       FROM products AS p
+       LEFT JOIN brands AS b ON p.brand_id = b.id
+       WHERE p.id = ${productId}
+     `);
+  return data;
+};
+
+const getProductHistories = async (productId) => {
+  return await database.query(`
+       SELECT 
+         p.id AS productId,
+         (SELECT 
+           JSON_ARRAYAGG(
+           JSON_OBJECT(
+             "price", dh.price,
+             "date", dh.created_at
+           )
+         )
+         FROM deal_histories AS dh
+         WHERE dh.product_id = ${productId}) AS complete,
+         (SELECT 
+           JSON_ARRAYAGG(
+             JSON_OBJECT(
+               "price", bp.price,
+               "date", bp.created_at
+             )
+           )
+         FROM buy_prices AS bp
+         WHERE bp.product_id = ${productId}) AS buy,
+         (SELECT 
+           JSON_ARRAYAGG(
+             JSON_OBJECT(
+               "price", sp.price,
+               "date", sp.created_at
+             )
+           )
+         FROM sell_prices AS sp
+         WHERE sp.product_id = ${productId}) AS sell
+       FROM products AS p
+       WHERE p.id = ${productId}
+     `);
+};
+
+const getBrandProduct = async (brandName) => {
+  return await database.query(`
+       SELECT 
+       p.id,
+       b.english_name AS brandName,
+       p.english_name AS productEngName,
+       p.thumbnail AS productImage,
+       SUBSTRING_INDEX(GROUP_CONCAT(bp.price ORDER BY bp.price ASC), "," , "1") AS price
+       FROM products AS p
+       LEFT JOIN brands AS b ON p.brand_id = b.id
+       LEFT JOIN buy_prices AS bp ON bp.product_id = p.id
+       WHERE b.english_name = "${brandName}"
+       GROUP BY p.id
+     `);
+};
+
+module.exports = {
+  getNewProducts,
+  getProductInfo,
+  getProductHistories,
+  getBrandProduct,
+};
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,7 +1,10 @@
-const productController = require('../controllers/productController');
+const productController = require("../controllers/productController");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('', productController.getNewProducts);
+router.get("", productController.getNewProducts);
+router.get("/info/:productId", productController.getProductInfo);
+router.get("/graph/:productId", productController.getProductHistories);
+router.get("/relation/:brandname", productController.getBrandProduct);
 
-module.exports = { router }
+module.exports = { router };
